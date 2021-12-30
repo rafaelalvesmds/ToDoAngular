61eb6cda4f59282fc7df1d656dd74f0caf0424c0
rafaelalvesmds <rafaelalvesmds.dev@gmail.com>
2021-12-30 16:24:23 -0300

    Lendo os Itens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,24 +5,26 @@ import { Todo } from 'src/models/todo.model';
 @Component({
   selector: 'app-root', //<app-root>
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
 export class AppComponent {
   public todos: Todo[] = [];
   public title: String = 'Minhas Tarefas';
   public form!: FormGroup;
 
-
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
-      title: ['', Validators.compose([
-        Validators.minLength(3),
-        Validators.maxLength(50),
-        Validators.required
-      ])]
+      title: [
+        '',
+        Validators.compose([
+          Validators.minLength(3),
+          Validators.maxLength(50),
+          Validators.required,
+        ]),
+      ],
     });
 
+    this.load();
   }
 
   remove(todo: Todo) {
@@ -30,17 +32,20 @@ export class AppComponent {
     if (index !== -1) {
       this.todos.splice(index, 1);
     }
+    this.save();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save();
   }
 
-  add(){
+  add() {
     // this.form.value => { title: 'Titulo'}
     const title = this.form.controls['title'].value;
     const id = this.todos.length + 1;
@@ -49,12 +54,19 @@ export class AppComponent {
     this.clear();
   }
 
-  clear(){
+  clear() {
     this.form.reset();
   }
 
-  save(){
+  save() {
+    //JSON > String
     const data = JSON.stringify(this.todos);
-    localStorage.setItem('todos', data)
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    //String > JSON
+    const data = localStorage.getItem('todos');
+    this.todos = JSON.parse(data!);
   }
 }
