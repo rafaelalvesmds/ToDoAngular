910112f4253404466db8558e76f5070cb9481812
rafaelalvesmds <rafaelalvesmds.dev@gmail.com>
2021-12-30 12:58:17 -0300

    Exibindo Dados {{}}

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,16 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   public todos: any[] = [];
+  public title: String = 'Minhas Tarefas';
 
 
   constructor() {
-    this.todos.push('study')
-    this.todos.push({message: 'json' })
-    this.todos.push(1995)
+    this.todos.push('study');
+    this.todos.push({"message": "json" });
+    this.todos.push(1995);
+  }
+
+  alterarTitulo() {
+    this.title = "Mudei"
   }
 }
